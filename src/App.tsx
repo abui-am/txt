e7e957142d7e989584533cb681f3da416b18f383
Abui <adjiem31@gmail.com>
2022-10-22 22:55:43 +0700

    feat: test

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import './App.css';
+import Confetti from 'react-confetti';
+import { useWindowSize } from 'react-use';
+import bongoCat from './assets/bongo-cat-transparent.gif';
+import { AES, enc } from 'crypto-js';
+function App() {
+  const { width, height } = useWindowSize();
+
+  const query = new URLSearchParams(window.location.search);
+  // console.log(query.get('txt') || '', 'magnet');
+  // console.log(decryptWithAES(query.get('txt') || ''));
+
+  // console.log(
+  //   'here',
+  //   encodeURIComponent(
+  //     encryptWithAES('<span>Semagat Nugasnya Firyall <br/> <br/> 💪💪💪</span>')
+  //   )
+  // );
+  return (
+    <>
+      <div className='App'>
+        <Confetti width={width} height={height} />
+        <img
+          width='100%'
+          style={{
+            maxWidth: '500px',
+          }}
+          src={bongoCat}
+          className='App-logo'
+          alt='logo'
+        />
+        <h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: decodeURIComponent(
+                decryptWithAES(query.get('txt') || '')
+              ),
+            }}
+          />
+
+          <br />
+        </h1>
+        <h2>-A</h2>
+      </div>
+    </>
+  );
+}
+
+const encryptWithAES = (text: string) => {
+  const passphrase = 'kamukamu2';
+  return AES.encrypt(text, passphrase).toString();
+};
+
+const decryptWithAES = (ciphertext: string) => {
+  const passphrase = 'kamukamu2';
+  const bytes = AES.decrypt(ciphertext, passphrase);
+  const originalText = bytes.toString(enc.Utf8);
+  return originalText;
+};
+
+export default App;
